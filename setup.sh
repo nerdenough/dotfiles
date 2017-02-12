704b52e3d631d89aa00b7a41b56be8235321e0ac
Brendan Goodenough <brendan@goodenough.nz>
2017-02-12 17:33:00 +1300

    Break setup scripts into separate task files

--- setup.sh
+++ setup.sh
@@ -1,30 +1,3 @@
-VIM_PLUG=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-echo "Creating directories and files"
-mkdir -p ~/.config
-touch $HOME/.bashrc
-
-# NVM
-echo "Installing nvm"
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
-) && . "$NVM_DIR/nvm.sh"
-
-nvm install node
-nvm use node
-
-# Node
-echo "Installing node modules"
-npm install -g eslint
-
-# Vim Plug
-echo "Installing vim plug"
-curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs $VIM_PLUG
-
-# Add config
-echo "Copying dotfile config"
-cp -R ./nvim $HOME/.config
-
-echo "Done!"
+./tasks/node.sh
+./tasks/vim-plug.sh
+./tasks/config.sh
---
+++ tasks/config.sh
@@ -0,0 +1,3 @@
+# Copy config files to their correct locations
+mkdir -p $HOME/.config
+cp -R ./nvim $HOME/.config
